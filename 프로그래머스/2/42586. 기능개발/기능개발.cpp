296eee02910dc4100dc1efac54982c8cf9b09c36
Ari <127702882+AriSpace00@users.noreply.github.com>
2025-09-17 22:23:02 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds)
+{
+    queue<int> days;
+    
+    for (int i=0; i<progresses.size(); i++)
+    {
+        int day = 0;
+        int diff = 100 - progresses[i];
+        while (diff > speeds[i] * day)
+        {
+            day++;
+        }
+        
+        days.push(day);
+    }
+    
+    vector<int> answer;
+    
+    int minDay = days.front();
+    answer.push_back(1);
+    days.pop();
+    
+    while(!days.empty())
+    {
+        if (minDay < days.front())
+        {
+            minDay = days.front();
+            answer.push_back(1);
+            days.pop();
+        }
+        else
+        {
+            answer[answer.size() - 1]++;
+            days.pop();
+        }
+    }
+    
+    return answer;
+}
