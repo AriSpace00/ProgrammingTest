eed443a8e987a480fbb7799c272938a1d1abc8d7
Ari <127702882+AriSpace00@users.noreply.github.com>
2025-09-19 18:46:32 +0900

    [level 1] Title: 모의고사, Time: 0.10 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> answers) 
+{
+    vector<int> first = { 1, 2, 3, 4, 5 };
+    vector<int> second = { 2, 1, 2, 3, 2, 4, 2, 5 };
+    vector<int> third = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+    
+    vector<int> scores(3, 0);
+    for (int i=0; i<answers.size(); i++)
+    {
+        if(answers[i] == first[i % first.size()])
+            scores[0]++;
+        
+        if(answers[i] == second[i % second.size()])
+            scores[1]++;
+        
+        if(answers[i] == third[i % third.size()])
+            scores[2]++;
+    }
+    
+    int maxScore =0;
+    for(int i=0; i<scores.size(); i++)
+    {
+        if(maxScore < scores[i])
+            maxScore = scores[i];
+    }
+    
+    vector<int> answer;
+    for(int i=0; i<scores.size(); i++)
+    {
+        if(maxScore == scores[i])
+            answer.push_back(i + 1);
+    }
+    
+    return answer;
+}
